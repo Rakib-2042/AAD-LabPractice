8a3f6d90928110c93d12c3cad03fe26c383a3571
Rakib-2042 <20223656648rakib@juniv.edu>
2025-05-18 06:36:56 +0600

    last push after solve all problem.

---
+++ AADLab/src/algorithm/withOOP/DFS/DFS.java
@@ -0,0 +1,18 @@
+package algorithm.withOOP.DFS;
+
+public class DFS {
+
+	public static void main(String[] args) {
+		Graph graph = new Graph(7);
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 2);
+        graph.addEdge(1, 3);
+        graph.addEdge(1, 4);
+        graph.addEdge(2, 5);
+        graph.addEdge(2, 6);
+
+        System.out.println("Depth-First Search (DFS) traversal starting from vertex 0:");
+        graph.DFS(0);
+	}
+
+}
